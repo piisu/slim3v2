a6378ce66b703062fa2f4b3e85047667b6ad00ed
katsume <katsume@gmail.com>
2018-02-09 13:16:14 +0900

    TestEnvironmentのgetModuleIdメソッドを実装

--- slim3/src/main/java/org/slim3/tester/TestEnvironment.java
+++ slim3/src/main/java/org/slim3/tester/TestEnvironment.java
@@ -245,4 +245,9 @@ public class TestEnvironment implements ApiProxy.Environment {
     public void setRemainingMillis(long remainingMillis) {
         this.remainingMillis = remainingMillis;
     }
+
+    @Override
+    public String getModuleId() {
+        return "slim3";
+    }
 }
